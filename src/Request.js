e6ae1f4099696a687dfa8e4733df53fe4722e3c3
gift56 <efeasiughu@gmail.com>
2022-07-28 15:52:33 +0100

    working on the api keys

--- src/Request.js
+++ src/Request.js
@@ -3,4 +3,6 @@ const Key = "83be94de5ce8714624cf13462ecb0357";
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=&language=enUS&page1`,
   requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${Key}&language=enUS&page1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/top_rated?api_key=${Key}&language=enUS&page2`,
+  requestUpComing: `https://api.themoviedb.org/3/movie/upcoming?api_key=${Key}&language=enUS&page1`,
 };
