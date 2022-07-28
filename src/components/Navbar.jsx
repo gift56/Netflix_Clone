b4895dad6a3d5a307a312bb5cf357ac364977fd0
gift56 <efeasiughu@gmail.com>
2022-07-28 14:53:29 +0100

    started working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="text-white">
+      <h1>NETFLIX</h1>
+      <div>
+        <button>Sign In</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
