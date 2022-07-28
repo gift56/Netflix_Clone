74b9901abfe3368b51f2a4e4984d3f541ee615b9
gift56 <efeasiughu@gmail.com>
2022-07-28 16:52:05 +0100

    done on the home page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -14,7 +14,18 @@ const Main = () => {
   }, []);
   console.log(movie);
 
-  return <div>Main</div>;
+  return (
+    <div className="w-full h-[550px] text-white">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        <img
+          src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
+          alt={movie?.title}
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
