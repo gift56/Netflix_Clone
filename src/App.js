689c3ecc5fae69d16d447aa3e251263f15edd64b
gift56 <efeasiughu@gmail.com>
2022-07-28 15:05:48 +0100

    wrapped our entire page with browser router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
