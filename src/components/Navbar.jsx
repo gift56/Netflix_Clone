a2272175479897277f7bc5dbd4378edb085f00e6
gift56 <efeasiughu@gmail.com>
2022-07-28 14:59:05 +0100

    done stying sign up btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
+      <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
+        NETFLIX
+      </h1>
       <div>
-        <button >Sign In</button>
-        <button className="bg-red-600 px-6 py-2">Sign Up</button>
+        <button>Sign In</button>
+        <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+          Sign Up
+        </button>
       </div>
     </div>
   );
