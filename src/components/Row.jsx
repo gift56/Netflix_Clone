5e5468465363c43f0efc602a431af7503e89f6f3
gift56 <efeasiughu@gmail.com>
2022-07-29 11:09:23 +0100

    positioned text

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -27,7 +27,9 @@ const Row = ({ title, fetchUrl }) => {
                 alt={item?.title}
               />
               <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                <p>{item?.title}</p>
+                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+                  {item?.title}
+                </p>
               </div>
             </div>
           ))}
