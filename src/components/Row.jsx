c3c62dbe3fa840ea0d868cf380f6038feb678e93
gift56 <efeasiughu@gmail.com>
2022-07-29 11:06:49 +0100

    done adding overlay

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -22,10 +22,13 @@ const Row = ({ title, fetchUrl }) => {
               className="relative p-2 inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px]"
             >
               <img
+                className="w-full h-auto block"
                 src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
                 alt={item?.title}
               />
-              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 text-white"></div>
+              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                <p>{item?.title}</p>
+              </div>
             </div>
           ))}
         </div>
