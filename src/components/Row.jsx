21a49e1ec82ed496aed0aca88aa395e6fd6f072b
gift56 <efeasiughu@gmail.com>
2022-07-29 13:11:30 +0100

    created slider show

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -12,12 +12,20 @@ const Row = ({ title, fetchUrl }) => {
     });
   }, [fetchUrl]);
 
-  console.log(movies);
+  const slideLeft = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center group">
         <MdChevronLeft
+          onClick={slideLeft}
           size={40}
           className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
         />
@@ -30,6 +38,7 @@ const Row = ({ title, fetchUrl }) => {
           ))}
         </div>
         <MdChevronRight
+          onClick={slideRight}
           size={40}
           className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden  group-hover:block"
         />
