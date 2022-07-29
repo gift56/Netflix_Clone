dc71e33bc7157c3750d31cdc3923a20cab285a3d
gift56 <efeasiughu@gmail.com>
2022-07-29 09:14:59 +0100

    positioning our buttons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -23,7 +23,7 @@ const Main = () => {
           alt={movie?.title}
           className="w-full h-full object-cover"
         />
-        <div>
+        <div className="absolute w-full top-[20%]">
           <div>
             <button className="border bg-gray-300 text-black py-2 px-5 border-gray-300">
               Play
