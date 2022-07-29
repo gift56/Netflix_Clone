2748c63748d5ba0d5ed7811b3a5c11b292238742
gift56 <efeasiughu@gmail.com>
2022-07-29 12:54:12 +0100

    fixed scrolling behaviour

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -5,7 +5,6 @@ import Movie from "./Movie";
 
 const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
-  
 
   useEffect(() => {
     axios.get(fetchUrl).then((res) => {
@@ -18,9 +17,12 @@ const Row = ({ title, fetchUrl }) => {
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center">
-        <div id={"slider"}>
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+        >
           {movies.map((item, id) => (
-            <Movie key={id} item={item}/>
+            <Movie key={id} item={item} />
           ))}
         </div>
       </div>
