fcfd89903a149b6d343b87abb6ecf872a92e5300
gift56 <efeasiughu@gmail.com>
2022-07-29 10:37:57 +0100

    added title area

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const Row = () => {
-  return <div>Row</div>;
+const Row = ({ title, fetchUrl }) => {
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+    </>
+  );
 };
 
 export default Row;
