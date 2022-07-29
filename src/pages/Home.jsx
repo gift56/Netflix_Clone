e10959c54c847e0995bd40b2ab7c72ed2dd62563
gift56 <efeasiughu@gmail.com>
2022-07-29 09:50:31 +0100

    fixed popular area

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Request";
 
 const Home = () => {
   return (
     <>
       <Main />
+      <Row title="Up Coming" fetchUrl={requests.requestUpComing} />
+      <Row title="Popular" fetchUrl={requests.requestPopular} />
+      <Row title="Up Coming" fetchUrl={requests.requestUpComing} />
+      <Row title="Up Coming" fetchUrl={requests.requestUpComing} />
     </>
   );
 };
