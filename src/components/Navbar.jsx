4b58e91eaa9cf0bf0d6fd99988b33adde07ab8a9
gift56 <efeasiughu@gmail.com>
2022-08-01 13:26:34 +0100

    writing some logic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
-  console.log(user);
+  // console.log(user);
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
@@ -12,16 +12,29 @@ const Navbar = () => {
           NETFLIX
         </h1>
       </Link>
-      <div>
-        <Link to="/login">
-          <button className="text-white pr-4">Sign In</button>
-        </Link>
-        <Link to="/signup">
-          <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
-            Sign Up
-          </button>
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
