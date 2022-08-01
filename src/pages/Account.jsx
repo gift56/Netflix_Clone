4eeb05a1ebb4107f16dc9ecdab3f60cc1369c573
gift56 <efeasiughu@gmail.com>
2022-08-01 16:00:47 +0100

    done with the account

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -10,8 +10,8 @@ const Account = () => {
           alt="/"
         />
         <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
-        <div>
-          
+        <div className="absolute top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl">My Shows</h1>
         </div>
       </div>
     </>
