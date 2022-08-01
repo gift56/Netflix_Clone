01b4bcf374d4b0f1d7f006154cd82ad140ed2569
gift56 <efeasiughu@gmail.com>
2022-08-01 14:00:00 +0100

    done with the js creation

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
-const ProtectedRoute = () => {
-  return (
-    <div>ProtectedRoute</div>
-  )
-}
+const ProtectedRoute = ({ children }) => {
+  const { user } = UserAuth();
+  if (!user) {
+    return <Navigate to="/" />;
+  } else {
+    return children;
+  }
+};
 
-export default ProtectedRoute
+export default ProtectedRoute;
