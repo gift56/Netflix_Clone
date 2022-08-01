c0dca978fc39b25528c3539bfb6a2249ccbaafc8
gift56 <efeasiughu@gmail.com>
2022-08-01 13:33:50 +0100

    added a function to navigate

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
   const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
 
-  const handleLogout = async () => {};
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
@@ -18,11 +26,9 @@ const Navbar = () => {
           <Link to="/account">
             <button className="text-white pr-4">Account</button>
           </Link>
-          <Link to="/signup">
-            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
-              Logout
-            </button>
-          </Link>
+          <button onClick={handleLogout} className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+            Logout
+          </button>
         </div>
       ) : (
         <div>
