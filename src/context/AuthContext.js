64a947218d70c226a905896e0e2c4c750c5e7df3
gift56 <efeasiughu@gmail.com>
2022-08-01 14:35:28 +0100

    database not responing

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -16,7 +16,7 @@ export function AuthContextProvider({ children }) {
   function signUp(email, password) {
     createUserWithEmailAndPassword(auth, email, password);
     setDoc(doc(db, "users", email), {
-      saveMovies: [],
+      savedShows: [],
     });
   }
 
