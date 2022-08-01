283a530e851d2228d8717cc0a0bd39cf1459b7cc
gift56 <efeasiughu@gmail.com>
2022-08-01 15:58:24 +0100

    working onaccount page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Account = () => {
   return (
-    <div>
-      <p></p>
-    </div>
-  )
-}
+    <>
+      <div className="w-full text-white">
+        <img
+          className="w-full h-[400px] object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/c2578c37-8569-4f88-b8f1-67a26a9ddcdd/f5f9d57b-8d7b-494f-a5ac-712f75c1217d/NG-en-20220725-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+        <div>
+          
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Account
+export default Account;
