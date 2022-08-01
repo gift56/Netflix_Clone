1bf9d27fb96681d6746576601192915b42f95a96
gift56 <efeasiughu@gmail.com>
2022-08-01 13:53:44 +0100

    added an error msg to sign in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,11 +11,13 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError("");
     try {
       await logIn(email, password);
       navigate("/");
     } catch (error) {
       console.log(error);
+      setError(error.message);
     }
   };
   return (
@@ -31,6 +33,7 @@ const Login = () => {
           <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
             <div className="max-w-[320px] mx-auto py-16">
               <h1 className="text-3xl font-bold">Sign In</h1>
+              {error ? <p className="text-red-500 font-bold my-2">{error}</p> : null}
               <form
                 onSubmit={handleSubmit}
                 className="w-full flex flex-col py-4"
