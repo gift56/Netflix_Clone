2f0a309cc823932d7ea6e37e7fe5162ff13417a0
gift56 <efeasiughu@gmail.com>
2022-08-01 16:10:01 +0100

    importing required stuff

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { UserAuth } from "../context/AuthContext";
+import { db } from "../firebase";
+import { updateDoc, doc, onSnapshot } from "firebase/firestore";
 const SavedShows = () => {
+  const { user } = UserAuth();
+  const slideLeft = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    let slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
   return (
-    <div>SavedShows</div>
-  )
-}
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">My Shows</h2>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          onClick={slideLeft}
+          size={40}
+          className="bg-white rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+        />
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
+          {movies.map((item, id) => (
+            <div
+              key={id}
+              className="relative p-2 inline-block cursor-pointer w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px]"
+            >
+              <img
+                className="w-full h-auto block"
+                src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
+                alt={item?.title}
+              />
+              <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+                  {item?.title}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <MdChevronRight
+          onClick={slideRight}
+          size={40}
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden  group-hover:block"
+        />
+      </div>
+    </>
+  );
+};
 
-export default SavedShows
+export default SavedShows;
