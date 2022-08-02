326559f372b090b1c561e84b8ce12bc1ff0ae42e
gift56 <efeasiughu@gmail.com>
2022-08-02 10:12:54 +0100

    made some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,26 +17,33 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
-        <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
-          NETFLIX
+        <h1 className="text-red-600 sm:text-4xl font-bold cursor-pointer uppercase">
+          MovieWorld
         </h1>
       </Link>
       {user?.email ? (
         <div>
           <Link to="/account">
-            <button className="text-white pr-4">Account</button>
+            <button className="text-white pr-4">
+              Account
+            </button>
           </Link>
-          <button onClick={handleLogout} className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+          <button
+            onClick={handleLogout}
+            className="sm:bg-red-600 sm:px-6 sm:py-2 sm:rounded cursor-pointer text-white"
+          >
             Logout
           </button>
         </div>
       ) : (
         <div>
           <Link to="/login">
-            <button className="text-white pr-4">Sign In</button>
+            <button className="text-white pr-4">
+              Sign In
+            </button>
           </Link>
           <Link to="/signup">
-            <button className="bg-red-600 px-6 py-2 rounded cursor-pointer text-white">
+            <button className="sm:bg-red-600 sm:px-6 sm:py-2 sm:rounded cursor-pointer text-white">
               Sign Up
             </button>
           </Link>
