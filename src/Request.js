de33ab3a503a01b90c1bf7263812ccdff0a8a82b
gift56 <efeasiughu@gmail.com>
2022-08-02 09:45:14 +0100

    changes body bg

--- src/Request.js
+++ src/Request.js
@@ -1,11 +1,11 @@
 const Key = "83be94de5ce8714624cf13462ecb0357";
 
 const requests = {
-  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${Key}&language=en-US&page1`,
-  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${Key}&language=en-US&page1`,
-  requestTrending: `https://api.themoviedb.org/3/movie/top_rated?api_key=${Key}&language=en-US&page3`,
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${Key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${Key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/top_rated?api_key=${Key}&language=en-US&page=2`,
   requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${Key}&language=en-US&query=horror&page=1&include_adult=false`,
-  requestUpComing: `https://api.themoviedb.org/3/movie/upcoming?api_key=${Key}&language=en-US&page1`,
+  requestUpComing: `https://api.themoviedb.org/3/movie/upcoming?api_key=${Key}&language=en-US&page=1`,
 };
 
 export default requests;
