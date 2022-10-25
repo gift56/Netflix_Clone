0acdfe2e8ae51a16d89dedb8c59c29460d56a99c
gift56 <efeasiughu@gmail.com>
2022-10-25 11:48:39 +0100

    added some stuff

--- src/pages/Getacc.jsx
+++ src/pages/Getacc.jsx
@@ -5,7 +5,7 @@ import { UserAuth } from "../context/AuthContext";
 const Getacc = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { user, signUp } = UserAuth();
+  const { signUp } = UserAuth();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
